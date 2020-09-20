ec8a563ca79868a6a80837dd476090cea14255ae
TonYem <antidota2700@gmail.com>
2020-09-20 19:27:54 +0300

    feat: done hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  // object for the final result
+  let result = {
+    'turns': 0,
+    'seconds': 0
+  };
+
+  // minimum turns formula
+  const minTurns = Math.pow(2, disksNumber) - 1;
+  result.turns = minTurns;
+
+  // calculate the minimum needed seconds and round down
+  const secsInHour = 3600;
+  let minTime = Math.floor(secsInHour / turnsSpeed * minTurns);
+  result.seconds = minTime;
+
+  return result;
+
 };
