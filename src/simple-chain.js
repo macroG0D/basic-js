c063504ab5cd4f3486d813e2f37ec72032db6f9a
TonYem <antidota2700@gmail.com>
2020-09-20 19:28:59 +0300

    feat: done simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    // check if input is correct, else reset chain and throw error
+    if (isNaN(position) || position < 1 || position > this.chain.length - 1) {
+      // reset current chain to prevent incorrect results on the next object.chain call
+      this.chain = [];
+      throw 'Error';
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let final = this.chain.slice();
+    // reset current chain to prevent incorrect results on the next object.chain call
+    this.chain = [];
+    return final.join('~~');
   }
 };
 
