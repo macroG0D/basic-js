74af94c0a8c4027a950ab44a6b5ec4751aea0626
TonYem <antidota2700@gmail.com>
2020-09-20 19:26:45 +0300

    feat: done dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let firstLetterArr = [];
+
+  // if not array — return false 
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  // iterate throught members 
+  for (let i = 0; i < members.length; i++) {
+    // check if correct type
+    if (typeof(members[i]) == 'string') {
+      // iterate throught the symbols of the string and push the first found letter
+      for (let k = 0; k < members[i].length; k++) {
+        // check if char is letter with regexp
+        if (members[i][k].match(/[a-z]/i)) {
+          firstLetterArr.push(members[i][k].toUpperCase());
+          // if first letter found, exit from the loop
+          break;
+        }
+      }
+    }
+  }
+
+  // return sorted array's string
+  return firstLetterArr.sort().join('');
 };
