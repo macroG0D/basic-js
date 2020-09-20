dbdcf2bf4e1989bd1a58e0aa3c976ca2326cca28
TonYem <antidota2700@gmail.com>
2020-09-20 19:22:58 +0300

    feat: done carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  // check inadequate values
+  if (sampleActivity > MODERN_ACTIVITY || sampleActivity < 1) {
+    return false;
+  // check correct type 
+  } else if (typeof (sampleActivity) !== 'string') {
+    return false;
+  // check if can be parsed to float
+  } else if (isNaN(parseFloat(sampleActivity))) {
+    return false;
+  } else {
+    // convert string number to float
+    sampleActivity = parseFloat(sampleActivity);
+    // use formula from example at https://chem.libretexts.org/Bookshelves/Physical_and_Theoretical_Chemistry_Textbook_Maps/Supplemental_Modules_(Physical_and_Theoretical_Chemistry)/Nuclear_Chemistry/Nuclear_Kinetics/Half-Lives_and_Radioactive_Decay_Kinetics#section_2
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    // round up the result
+    let t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
+    return t;
+  }
 };
